b3a7b65521ff8c6e569c8c1e02f9b2c9aaaa0dda
jgotti1 <96301204+jgotti1@users.noreply.github.com>
2022-09-20 14:15:42 -0400

    added mongoose and DB connection

--- server/server.js
+++ server/server.js
@@ -1,17 +1,23 @@
 // Package depencies
 const express = require("express");
-require("dotenv").config()
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 // Server setup
-const PORT = process.env.PORT | 5000
+const PORT = process.env.PORT | 5000;
 const app = express();
+app.use(cors());
 
 // Routes
 app.get("/", (req, res) => {
-    res.send("TaskMaster")
-})
+  res.send("TaskMaster");
+});
+
+// DB Connect
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Listening
 app.listen(PORT, () => {
-    console.log("Server listening at", PORT)
-})
+  console.log("Server listening at", PORT);
+});
