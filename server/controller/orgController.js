de05b3687cba6044322f48cab19cdf2d7c8ea6e5
JShinault0620 <jshincool@gmail.com>
2022-09-21 19:52:00 -0400

    Organization signup now creates admin user

--- server/controller/orgController.js
+++ server/controller/orgController.js
@@ -1,11 +1,18 @@
 import Organization from "../models/orgModel.js";
+import User from "../models/userModel.js";
 
 export const orgSignup = async (req, res) => {
-    const { name } = req.body;
+    const { name, email, password, first_name, last_name } = req.body;
 
     try {
-        const org = await Organization.signup(name);
-        res.send(org);
+        const org = new Organization({name: name});
+        try {
+            const user = await User.signup(email, password, org._id, first_name, last_name, true);
+            org.save();
+            res.send(user);
+        } catch(error) {
+            res.status(400).json({ error: error.message })
+        }
     } catch(error) {
         res.status(400).json({ error: error.message });
     };
@@ -14,8 +21,8 @@ export const orgSignup = async (req, res) => {
 export const findAllOrgs = async (req, res) => {
     try {
         const orgs = await Organization.find();
-        res.send(orgs)
+        res.send(orgs);
     } catch(error) {
-        res.status(400).json({ error: error.message })
+        res.status(400).json({ error: error.message });
     }
 }
