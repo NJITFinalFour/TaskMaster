2ecbf23c2a7ab6e6aeed8c982eb8d41afa51da03
JShinault0620 <jshincool@gmail.com>
2022-09-20 21:14:16 -0400

    Fixed warning caused by timer

--- client/src/components/Timer.jsx
+++ client/src/components/Timer.jsx
@@ -2,25 +2,26 @@ import { useState, useEffect } from "react";
 
 const Timer = () => {
     const dateNoComma = () => {
-        const date = new Date;
-        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
-    }
+        const date = new Date();
+        return `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+    };
 
     const [date, setDate] = useState(dateNoComma());
 
-    const timer = {
-      interval: null,
-      start: function() {
-        this.interval = setInterval(() => {
-          setDate(dateNoComma);
-        }, 1000);
-      },
-      stop: function() {
-        clearInterval(this.interval);
-      }
-    };
+    
   
     useEffect(() => {
+        const timer = {
+            interval: null,
+            start: function() {
+                this.interval = setInterval(() => {
+                setDate(dateNoComma);
+                }, 1000);
+            },
+            stop: function() {
+                clearInterval(this.interval);
+            }
+        };
       timer.start();
       return () => {
         timer.stop();
