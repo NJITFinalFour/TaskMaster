9db6b16be5bc7a888dd5959e12a162107df98718
JShinault0620 <jshincool@gmail.com>
2022-09-24 11:15:56 -0400

    findTasksByUser route

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,10 +1,14 @@
 import express from "express";
-import { getTask, createTask, deleteTask, /*deleteAllTasks,*/ updateTask, getOne } from "../controller/taskController.js";
+import { getTask, createTask, deleteTask, /*deleteAllTasks,*/ updateTask, getOne, findTasksByUser } from "../controller/taskController.js";
 import requireAuth from "../middleware/requireAuth.js";
+
+import bodyParser from "body-parser";
+const jsonParser = bodyParser.json();
 
 const router = express.Router();
 // router.use(requireAuth);
 router.get("/", getTask);
+router.get("/user", jsonParser, findTasksByUser);
 router.get("/:id", getOne);
 router.post("/", createTask);
 router.delete("/:id", deleteTask);
