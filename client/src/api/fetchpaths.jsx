a0c798dcfab90681642473702b1abdb1a14b200b
John Margotti <jgotti1@optonline.net>
2022-09-27 20:10:39 -0400

    adjuted fetch paths while tesing to deploy

--- client/src/api/fetchpaths.jsx
+++ client/src/api/fetchpaths.jsx
@@ -1,11 +1,11 @@
 // ++++++++++++++++ DEV PATHS ++++++++++++++++
 
-export const signupAdminFetchPath = "http://localhost:5000/organizations/signup"
-export const signupFetchPath = "http://localhost:5000/user/signup/";
-export const taskFetchPath = "http://localhost:5000/tasks/";
-export const loginFetchPath = "http://localhost:5000/user/login";
-export const orgFetchPath = "http://localhost:5000/organizations";
-export const userFetchPath = "http://localhost:5000/user/";
+// export const signupAdminFetchPath = "http://localhost:5000/organizations/signup"
+// export const signupFetchPath = "http://localhost:5000/user/signup/";
+// export const taskFetchPath = "http://localhost:5000/tasks/";
+// export const loginFetchPath = "http://localhost:5000/user/login";
+// export const orgFetchPath = "http://localhost:5000/organizations";
+// export const userFetchPath = "http://localhost:5000/user/";
 
 
 //+++++++++++++++++++Heroku PROD BACKEND PATHS ++++++++++++++++++++
