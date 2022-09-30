a461d23a25f37a70d548c6631504da7772149474
John Margotti <jgotti1@optonline.net>
2022-09-30 10:19:24 -0400

    login error handling added to front end

--- client/src/api/fetchpaths.jsx
+++ client/src/api/fetchpaths.jsx
@@ -1,6 +1,6 @@
 // ++++++++++++++++ DEV PATHS ++++++++++++++++
 
-// export const signupAdminFetchPath = "http://localhost:5000/organizations/signup"
+// export const signupAdminFetchPath = "http://localhost:5000/organizations/signup";
 // export const signupFetchPath = "http://localhost:5000/user/signup/";
 // export const taskFetchPath = "http://localhost:5000/tasks/";
 // export const loginFetchPath = "http://localhost:5000/user/login";
@@ -9,11 +9,9 @@
 
 //+++++++++++++++++++AWS PROD BACKEND PATHS ++++++++++++++++++++
 
-
 export const signupAdminFetchPath = "https://server.taskmasterusa.com/organizations/signup";
 export const signupFetchPath = "https://server.taskmasterusa.com/user/signup/";
 export const taskFetchPath = "https://server.taskmasterusa.com/tasks/";
 export const loginFetchPath = "https://server.taskmasterusa.com/user/login";
 export const orgFetchPath = "https://server.taskmasterusa.com/organizations";
 export const userFetchPath = "https://server.taskmasterusa.com/user/";
-
